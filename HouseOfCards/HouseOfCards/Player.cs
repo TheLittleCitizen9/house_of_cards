53b8a1ded2732a66aa0b8ed35989f7786bb682ae
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-31 09:23:26 +0300

    Created Player class

---
+++ HouseOfCards/HouseOfCards/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HouseOfCards
+{
+    public class Player
+    {
+        public string Name { get; private set; }
+        public Card[] Cards { get; set; }
+
+        public Player(string name)
+        {
+            Name = name;
+            Cards = new Card[5];
+        }
+    }
+}
