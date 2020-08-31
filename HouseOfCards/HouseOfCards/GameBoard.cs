3915bbf7f2e08a16ead9e962ecf29fc85b23dd28
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-31 09:20:45 +0300

    Created GameBoard class

---
+++ HouseOfCards/HouseOfCards/GameBoard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HouseOfCards
+{
+    public class GameBoard
+    {
+        public Card[,] Board { get; }
+
+        public GameBoard()
+        {
+            Board = new Card[5, 5];
+        }
+
+        public void PutCardOnBoard(Card card)
+        {
+            for (int i = 0; i < Board.GetLength(0); i++)
+            {
+                for (int j = 0; j < Board.GetLength(1); j++)
+                {
+                    if (Board[i, j].Color == card.Color)
+                    {
+                        if(Board[i,j+1] == null)
+                        {
+                            Board[i, j + 1] = card;
+                            break;
+                        }
+                        
+                    }
+                }
+                
+            }
+        }
+    }
+}
