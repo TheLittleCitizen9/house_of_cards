b2f2b265e5d15e5ea1b92ce61f8d80b7984e3179
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-31 09:00:15 +0300

    Create Card class

---
+++ HouseOfCards/HouseOfCards/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HouseOfCards
+{
+    public class Card
+    {
+        public int Number { get; set; }
+        public CardColors Color { get; set; }
+
+        public Card(int number, CardColors cardColor)
+        {
+            Number = number;
+            Color = cardColor;
+        }
+    }
+}
---
+++ HouseOfCards/HouseOfCards/CardColors.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HouseOfCards
+{
+    public enum CardColors
+    {
+        Red,
+        Green,
+        Blue,
+        Yellow,
+        White
+    }
+}
